c932f18ec8ce09cfaa0ec808f3c2734a7593a047
Mateus Eloi da Silba Bastos <eluire@gmail.com>
2018-08-27 16:58:29 -0300

    add exe

--- lista_PA/Exercício_2.c
+++ lista_PA/Exercício_2.c
@@ -7,21 +7,23 @@
         float *p, *q;
         p = &i;
         q = &j;
-        printf("%x %x ", p,q);
-// o ponteiro p recebe o endereço de memoria em que a variável i está armazenada. o mesmo vale para
+//o ponteiro p recebe o endereço de memoria em que a variável i está armazenada. o mesmo vale para
 // ponteiro p .
-        p == &i;
+       if (p == &i){
+            printf("verdaderiro\n" );
+        }
+        else{
+            printf("falso\n");
+        }
 // o oque está dentro do ponteiro é o endereço da variável i.
-        *p - *q;
+        printf("%f\n",*p - *q);
 // o que está dentro do endereço que p aponta meno oque está dentro do endereço que q aponta
-        **&p;
+        printf("%f\n",**&p);
 // 1- &p revela o endereço que o ponteiro p é armazenado, 2- *&p revela oq esta dentro desse endereço que
-// por sua vez é o endereço da variável i(variável que ele está apontando), 3- **&p revela oque tem dentro
+// por sua vez é o endereço da variável (variável que ele está apontando), 3- **&p revela oque tem dentro
 // do endereço que p aponta, justamente a variável 1.
-       a = (3 - *p/(*q) + 7);
+       printf("%f\n",(3 - *p/(*q) + 7));
 // a vai receber 3 - (o valor que está dentro do endereço que p aponta dividido pelo valor dentro do
 //endereço que q aponta)+ 7.
-       printf("%f",a);
-       printf("\n %d",sizeof(float));
     return 0;
     }
--- lista_PA/Exercício_4.c
+++ lista_PA/Exercício_4.c
@@ -2,12 +2,23 @@
 
 int main()
 {
-	
+	int i,j,*p,*q;
 
-
-
-
-
+	p = i;
+	// ERRADO p só pode armazenar endereços 
+	q = &j;
+	// CORRETO
+	p = &*&i;
+	//CORRETO
+	i = (*&)j;
+	// ERRADO, parenteses mal colocado 
+	i = *&j;
+	// CORRETO
+	i = *&*&j;
+	// CORRETO
+	q = *p;
+	// ERRADO,ponteiro só armazena endereços
+	i = (*p)++ + *q;
 	
 	return 0;
 }
