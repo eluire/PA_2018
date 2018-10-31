289132302078fd31063bb59a2d58c9d20e802eb4
Hadênia Rodrigues <42811357+Hadenia@users.noreply.github.com>
2018-10-30 23:15:36 -0300

    Update retangulo.h

--- proj_2_PA/retangulo.h
+++ proj_2_PA/retangulo.h
@@ -1,13 +1,30 @@
 #ifndef RETANGULO_H
 #define RETANGULO_H
 #include "figura_geometrica.h"
-
+/**
+ * @brief Retangulo é classe derivada de figura_geometrica
+ */
 class Retangulo: public Figura_geometrica{
 
 private:
+     /**
+   * @brief xi cordenada x inicial do retangulo
+   * @brief yi cordenada y inicial do retangulo
+   * @brief altura altura do retangulo
+   * @brief largura largura do retangulo
+   */
   int xi,yi,altura,largura;
   bool fillmode;
 public:
+   /**
+   * @brief Retangulo construtor da classe, com valores iniciais de parametros
+   * @param _xi
+   * @param _yi
+   * @param _altura
+   * @param _largura
+   * @param fillmode
+   * @param brush
+   */
   Retangulo(int _xi = 0, int _yi =0 , int _altura =0,int _largura =0 ,bool fillmode = 0,char brush = 'X');
   void draw(Screen &t);
 };
