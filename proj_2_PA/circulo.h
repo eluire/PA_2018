111f92906bfc0b7f464672edaf9aac04b4c9e24e
Hadênia Rodrigues <42811357+Hadenia@users.noreply.github.com>
2018-10-30 23:13:57 -0300

    Update circulo.h

--- proj_2_PA/circulo.h
+++ proj_2_PA/circulo.h
@@ -1,16 +1,35 @@
 #ifndef CIRCULO_H
 #define CIRCULO_H
 #include "figura_geometrica.h"
-
+/**
+ * @brief The Circulo class derivada da classe Figura geometrica
+ */
 class Circulo : public Figura_geometrica{
 
-  private:
+private:
+    /**
+     * @brief xc posição x do centro
+     * @brief yc posição y do centro
+     * @brief raio valor do raio
+     */
     int xc,yc,raio;
     bool fillmode;
 
   public:
 
+    /**
+     * @brief Circulo construtor com parametros iniciais da classe inicialmente predefinidos
+     * @param _xc
+     * @param _yc
+     * @param _raio
+     * @param _fillmode
+     * @param brush
+     */
     Circulo(int _xc, int _yc,int _raio, bool _fillmode,char brush);
+    /**
+     * @brief draw desenhar na tela
+     * @param t
+     */
     void draw(Screen &t);
 
 
